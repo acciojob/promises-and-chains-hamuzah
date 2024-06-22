8599f3e7f8e4dc410404e5ea700bce125666703e
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-06-22 10:34:32 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-//your JS code here. If required.
-let formbutton = document.getElementById("btn")
-
-function promiseApi(name,age){
-    return new Promise((resolve,reject)=>{
-         if(age > 18){
-            setTimeout(()=>{
-                resolve(
-                    alert(`welcome, ${name}, You can Vote`)
-                )
-            },4000)
-         }else{
-            reject(
-                 alert(`Oh Sorry, ${name}, You aren't old enough`)
-             )
-         }
-    })
-}
-
-formbutton.addEventListener("click",(event)=>{
-   event.preventDefault()
-    let name = document.getElementById("name").value
-    let age = document.getElementById("age").value
-    //validation
-    if(name == "" && age == ""){
-        alert("please fill all the fields")
-    }else{
-        promiseApi(name,age)
+let formbutton = document.getElementById("btn");
+function promiseApi1(name , age)
+{
+ return new Promise((resolve , reject) => {
+    if(age > 18)
+    {
+        setTimeout(() => {
+            resolve(
+                alert(`welcome , ${name}, You can vote`)
+                // console.log(`You can vote , ${name}`)
+            )
+        } , 4000)
     }
-
+    else
+    {
+        alert(`Oh sorry ${name}. You aren't old enough.`)
+    }
+ })
+}
+formbutton.addEventListener("click" , (event) => {
+    event.preventDefault();
+    let nameinput = document.getElementById("name").value;
+    let ageinput = document.getElementById("age").value;
+    if (nameinput == "" && ageinput == "") {
+        alert("It should not be empty")
+    }
+    else
+    {
+        promiseApi1(nameinput , ageinput);
+    }
 })
